b411aee91016f2cdd6508c6cd642edeb5356f756
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-22 21:39:08 -0400

    add bookmark model interface

---
+++ models/bookmarks/Bookmark.ts
@@ -0,0 +1,16 @@
+/**
+ * @file Defines an interface for the bookmark model and schema
+ */
+import Tuit from "../tuits/Tuit";
+import User from "../users/User";
+
+/**
+ * @interface Bookmark represents a Bookmark object which relate a specific user to a specific tuit.
+ * @typedef Bookmark Represents a bookmark relationship between a user and a tuit
+ * @property {Tuit} tuit the tuit that the user wishes to bookmark
+ * @property {User} bookmarkedBy the user who is bookmarking the tuit
+ */
+export default interface Bookmark {
+    tuit: Tuit,
+    bookmarkedBy: User
+};
