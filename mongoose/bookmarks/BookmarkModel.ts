bf3daf300b8daee455d739b0ea5b6ede6153806a
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-22 22:02:51 -0400

    add mongo model and schema for bookmarks

---
+++ mongoose/bookmarks/BookmarkModel.ts
@@ -0,0 +1,9 @@
+/**
+ * @file Implements mongoose model for Bookmarks
+ */
+import mongoose from 'mongoose';
+import BookmarkSchema from "./BookmarkSchema";
+
+
+const BookmarkModel = mongoose.model('BookmarkModel', BookmarkSchema);
+export default BookmarkModel;
