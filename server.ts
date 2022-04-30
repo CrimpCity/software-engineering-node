5305ebd4b8a88e3ad899be7970f95116763ff717
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-30 19:09:19 -0400

    add Jose's auth controller

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@
  * Connects to a remote MongoDB instance hosted on the Atlas cloud database
  * service
  */
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import CourseController from "./controllers/CourseController";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
@@ -18,26 +18,49 @@ import LikeController from "./controllers/LikeController";
 import mongoose from "mongoose";
 var cors = require('cors')
 
+
+const session = require("express-session");
+const app = express();
+let sess = {
+    secret: process.env.SECRET,
+    proxy: true,
+    cookie: {
+        secure: true,
+        sameSite: 'none'
+    }
+}
+
+if (process.env.ENV === 'production') {
+    app.set('trust proxy', 1)
+    sess.cookie.secure = true;
+}
+
+app.use(session(sess));
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}));
+
+app.get('/hello', (req, res) =>
+    res.send('Hello World!'));
+
+app.get('/add/:a/:b', (req, res) => {
+    res.send(req.params.a + req.params.b);
+})
+
+
 // build the connection string
 const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
+const DB_USERNAME = "Georgian";
+const DB_PASSWORD = "Georgian";
+const HOST = "cluster0.a0awg.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
 mongoose.connect(connectionString);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
-
-app.get('/add/:a/:b', (req: Request, res: Response) =>
-    res.send(req.params.a + req.params.b));
 
 // create RESTful Web service API
 const courseController = new CourseController(app);
